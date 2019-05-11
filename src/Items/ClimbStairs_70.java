de94b8106ffec190ede1250eb17be3d7d304c9cb
hechuan73 <1044856164@qq.com>
2019-05-11 16:52:35 +0800

    Description: update solution for problem 70.

---
+++ src/Items/ClimbStairs_70.java
@@ -0,0 +1,15 @@
+package Items;
+
+public class ClimbStairs_70 {
+
+    /**
+     *
+     * @param n the total step of the stair
+     * @return the options to climb the stair.
+     */
+    public int climbStairs(int n) {
+
+        return 0;
+    }
+
+}
