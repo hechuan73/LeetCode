302afbc78c36522d661bb1a8f59e803034dfd55a
hechuan73 <1044856164@qq.com>
2019-05-14 16:43:30 +0800

    Description: update solution for problem 69.

---
+++ src/Items/Sqrtx_69.java
@@ -0,0 +1,24 @@
+package Items;
+
+public class Sqrtx_69 {
+    public static int mySqrt(int x) {
+        if (x == 1) return 1;
+        long left = 0, right = x;
+        while (left < right - 1) { // if right = left + 1, it will loop infinitely.
+            if (((left + right)/2) * ((left + right)/2) == (long) x) return (int)(left + right)/2;
+            else if (((left + right)/2) * ((left + right)/2) > (long) x) right = (left + right)/2;
+            else left = (left + right)/2;
+        }
+        if (left * left > (long) x) return (int) left-1;
+        return (int) left;
+    }
+
+//    public static int mySqrt(int x) {
+//        for (long i = 0; i <= (long) x /2 + 1; i++) {
+//            if (i*i == (long) x) return (int)i;
+//            else if (i*i > (long) x) return (int)i-1;
+//        }
+//
+//        return 1;
+//    }
+}
