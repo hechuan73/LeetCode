8e74e9b7ea0c5a866aade6700fa2d79de1dee909
hechuan73 <1044856164@qq.com>
2019-05-23 21:19:09 +0800

    Description: update solutions for problem 18 and 19.

---
+++ src/Items/RemoveNthNodeFromEndOfList_19.java
@@ -0,0 +1,34 @@
+package Items;
+
+public class RemoveNthNodeFromEndOfList_19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode headNode = new ListNode(0);
+        headNode.next = head;
+        ListNode first = headNode;
+        ListNode second = headNode;
+
+        for (int i = 0; i <= n; i++) first = first.next;
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+        return headNode.next;
+    }
+
+//    public ListNode removeNthFromEnd(ListNode head, int n) {
+//        List<ListNode> nodes = new ArrayList<>();
+//        ListNode node = head;
+//        while (node != null) {
+//            nodes.add(node);
+//            node = node.next;
+//        }
+//
+//        if (n == nodes.size()) head = head.next;
+//        else nodes.get(nodes.size() - n - 1).next = nodes.get(nodes.size() - n).next;
+//
+//        return head;
+//    }
+}
