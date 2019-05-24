a07bb757fb6ab328a910d72000ba906ba119698f
hechuan73 <1044856164@qq.com>
2019-05-24 14:29:20 +0800

    Description: update solutions for problem 29.

---
+++ src/Items/DivideTwoIntegers_29.java
@@ -0,0 +1,31 @@
+package Items;
+
+public class DivideTwoIntegers_29 {
+    public static int divide(int dividend, int divisor) {
+        boolean sign = (dividend < 0) ^ (divisor < 0);
+        if (dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+
+        long dividend1 = Math.abs((long)dividend);
+        long divisor1 = Math.abs((long)divisor);
+
+        int result = fastDivide(dividend1, divisor1);
+        return sign ? -result : result;
+    }
+
+    private static int fastDivide(long dividend, long divisor) {
+        if (divisor > dividend) return 0;
+
+        int multiple = 1;
+        long sum = divisor;
+
+        //  Find the largest multiple so that (divisor * multiple <= dividend),
+        //  whereas we are moving with stride 1, 2, 4, 8, 16...2^n for performance reason.
+        //  Think this as a binary search.
+        while (sum + sum <= dividend) {
+            sum += sum;
+            multiple += multiple;
+        }
+
+        return multiple + fastDivide(dividend - sum, divisor);
+    }
+}
