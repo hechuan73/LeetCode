7f638a37eefb11d16d3646f815ec6f5e3a67acf8
hechuan73 <1044856164@qq.com>
2019-05-30 09:41:19 +0800

    Description: update solutions for problem 46 and 47.

---
+++ src/Items/PermutationsII_47.java
@@ -0,0 +1,31 @@
+package Items;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PermutationsII_47 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int[] visited = new int[nums.length];
+        backTrace(nums, result, new ArrayList<>(), visited);
+        return result;
+    }
+
+    private void backTrace(int[] nums, List<List<Integer>> result, List<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length ; i++) {
+            if ((visited[i] == 1) || (i > 0 && visited[i-1] == 0 && nums[i] == nums[i-1])) continue;
+            tmp.add(nums[i]);
+            visited[i] = 1;
+            backTrace(nums, result, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size()-1);
+        }
+    }
+}
---
+++ src/Items/Permutations_46.java
@@ -0,0 +1,29 @@
+package Items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations_46 {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backTrace(nums, result, new ArrayList<>(), visited);
+        return result;
+    }
+
+    private void backTrace(int[] nums, List<List<Integer>> result, List<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length ; i++) {
+            if (visited[i] == 1) continue;
+            tmp.add(nums[i]);
+            visited[i] = 1;
+            backTrace(nums, result, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size()-1);
+        }
+    }
+}
