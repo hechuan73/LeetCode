d419349c206f3bc3263060183414608cbfac7c54
hechuan73 <1044856164@qq.com>
2019-06-24 09:06:54 +0800

    Description: update the solutions of problem 54, 59 and 94.

---
+++ src/Items/BinaryTreeInorderTraversal_94.java
@@ -0,0 +1,34 @@
+package Items;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * There are three approaches to traverse binary tree with inorder.
+ * 1. Recursive method.
+ * 2. Iterating method using stack.
+ * 3. Morris traversal.
+ *
+ * Here I use the 2nd method.
+ */
+public class BinaryTreeInorderTraversal_94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> elements = new Stack<>();
+
+        TreeNode cur = root;
+        while (cur != null || !elements.isEmpty()) {
+            while (cur != null) {
+                elements.push(cur);
+                cur = cur.left;
+            }
+
+            cur = elements.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+
+        return result;
+    }
+}
