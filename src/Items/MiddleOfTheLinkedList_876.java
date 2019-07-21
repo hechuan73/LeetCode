ef901eebe572ed043c267d01afb09561614b2c38
hechuan73 <1044856164@qq.com>
2019-07-21 22:12:56 +0800

    Description: update solutions for problem 155, 206, 232, 682, 844 and 876.

---
+++ src/Items/MiddleOfTheLinkedList_876.java
@@ -0,0 +1,16 @@
+package Items;
+
+public class MiddleOfTheLinkedList_876 {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
